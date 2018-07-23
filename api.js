f9f3bcac9863fc2b344869c9b6cc9373c10d0060
MoonHyuk <godmoon00@naver.com>
2018-07-24 08:04:52 +0900

    commit

--- api.js
+++ api.js
@@ -2,4 +2,4 @@ var query = decodeURIComponent(window.location.search.substring(1));
 var val = dict[query];
 console.log(query);
 console.log(val);
-document.body.innerHTML = val;
+document.body.innerHTML = JSON.stringify(val);
